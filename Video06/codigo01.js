47634f05c2b7cb53ddbc7ee18f0666a83812db87
Fernando Warno <ferwar@hotmail.com>
2022-12-23 02:59:25 -0300

    video06 completo

---
+++ Video06/codigo01.js
@@ -0,0 +1,80 @@
+// Operadores de asignación
+// x = 10;
+// Nunca "==", ni "===", ya que son operadores de comparación.
+// x += 4; // devolveria x = 14; (x = 10 + 4)
+// x -= 4; // devolveria x = 6; (x = 10 - 4)
+// x /= 4; // devolveria x = 2.5; (x = 10 / 4)
+// x *= 4; // devolveria x = 40; (x = 10 * 4)
+// x %= 4; // devolveria x = 2; El resto de la división entre 10 y 4 es 2.
+// x **= 4; // devolveria x = 10000; (x = 10 ** 4)
+
+// ***********************************************************************
+
+// Operadores aritméticos
+
+// y = x + 2; // x = 10, y = 12
+// y = x - 2; // x = 10, y = 8
+// y = x * 2; // x = 10, y = 20
+// y = x / 2; // x = 10, y = 5
+// y = x % 2; // x = 10, y = 0
+// y = x ** 2; // x = 10, y = 100
+// +"4"; // Devuelve 4
+// -"4"; // Devuelve -4
+
+// ***********************************************************************
+
+// Operador de incremento unitario antes de operar
+
+// a = 10;
+// x = ++a * 2; // x = 22, a = 11
+
+// Operador de incremento unitario después de operar
+
+// b = 10;
+// y = b++ * 2; // y = 20, b = 11
+
+// Operador de decremento unitario antes de operar
+
+// c = 10;
+// h = --c * 2; // h = 18, c = 9
+
+// Operador de decremento unitario después de operar
+
+// d = 10;
+// i = d-- * 2; // i = 20, d = 9
+
+// ***********************************************************************
+
+// Operadores de comparación
+
+// a == b; // a es igual a b
+// a === b; // a es igual a b y son del mismo tipo
+// a != b; // a es diferente de b
+// a !== b; // a es diferente de b o son de distinto tipo
+// a > b; // a es mayor que b
+// a < b; // a es menor que b
+// a >= b; // a es mayor o igual que b
+// a <= b; // a es menor o igual que b
+
+// ***********************************************************************
+
+// Operador Ternario
+
+// a = 10;
+// b = 20;
+// Condición ? Si es verdadero : Si es falso
+// c = a > b ? a : b; // c = 20
+
+// ***********************************************************************
+
+// Operadores lógicos
+
+// a = 10;
+// b = 20;
+// c = 30;
+// d = 40;
+// a > b && c > d; // false
+// a > b || c > d; // true
+// !(a > b); // true
+
+
